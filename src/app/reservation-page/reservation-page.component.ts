dc267a245d590369181ce52d67806a4cbd0a0dc9
Łukasz Staniszewski <lukasz.staniszewski@ppuc.pl>
2022-11-20 15:23:16 +0100

    add initial version of reservation page

--- src/app/reservation-page/reservation-page.component.ts
+++ src/app/reservation-page/reservation-page.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-const seats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] as const;
-
+const seatsCol = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+] as const;
+const seatsRow = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J'] as const;
 // type Seats =
 @Component({
   selector: 'app-reservation-page',
@@ -9,9 +11,11 @@ const seats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] as const;
   styleUrls: ['./reservation-page.component.css'],
 })
 export class ReservationPageComponent implements OnInit {
-  seatsNum: typeof seats;
+  seatsNum: typeof seatsCol;
+  seatsChar: typeof seatsRow;
   constructor() {
-    this.seatsNum = seats;
+    this.seatsNum = seatsCol;
+    this.seatsChar = seatsRow;
   }
 
   ngOnInit(): void {}
