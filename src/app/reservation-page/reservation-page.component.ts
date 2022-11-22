c33b007e906f388604e67a9160c7900f12eaa418
Łukasz Staniszewski <lukasz.staniszewski@ppuc.pl>
2022-11-22 16:47:37 +0100

    add screening room

--- src/app/reservation-page/reservation-page.component.ts
+++ src/app/reservation-page/reservation-page.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-const seatsCol = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-] as const;
-const seatsRow = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J'] as const;
+const seatsCol = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+const seatsRow = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J'];
 // type Seats =
 @Component({
   selector: 'app-reservation-page',
@@ -13,6 +11,7 @@ const seatsRow = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J'] as const;
 export class ReservationPageComponent implements OnInit {
   seatsNum: typeof seatsCol;
   seatsChar: typeof seatsRow;
+
   constructor() {
     this.seatsNum = seatsCol;
     this.seatsChar = seatsRow;
