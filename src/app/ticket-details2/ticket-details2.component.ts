0c7966b2410097e7cff2c7c008acd367ae90e7eb
Lukasz Staniszewski <l.staniszewski01@gmail.com>
2022-12-13 10:50:27 +0100

    changes

--- src/app/ticket-details2/ticket-details2.component.ts
+++ src/app/ticket-details2/ticket-details2.component.ts
@@ -10,42 +10,27 @@ import { TicketInfo, TicketService } from '../ticket/ticket.service';
 })
 export class TicketDetails2Component {
   maxTicketAmount = SET_UP.MAX_TICKETS_AMOUT_PER_USER;
-  options = [0, 1, 2, 3, 4, 5, 6, 7, 9, 10];
-  ticketForm = this.createForm();
+  options = [1, 2, 3, 4, 5, 6, 7, 9, 10];
+
   ticketsInfo!: TicketInfo[];
   hide = true;
   ticketType!: string;
   price!: number;
+  selectedBilets = new Set([]);
   constructor(
     private builder: NonNullableFormBuilder,
     private ticketService: TicketService
-  ) {
-    console.log(this.ticketForm);
-    //@ts-ignore
-    console.log(this.ticketForm.controls.tickets['normal']);
-  }
+  ) {}
 
   ngOnInit(): void {
-    // console.log(this.seatPosition);
     this.ticketService.getTicketInfo();
     this.ticketService.ticketInfo$.subscribe((ticketInfo) => {
       this.ticketsInfo = ticketInfo;
-      // this.biletTypesAmount = ticketInfo.map()
+
       if (!ticketInfo[0]) return;
-      for (let ticketInfo of this.ticketsInfo) {
-        this.ticketForm.controls.tickets.push(
-          this.builder.control(ticketInfo.type)
-        );
-        // form.addControl(ticketInfo.type, this.builder.control(''));
-      }
+
       this.mapDefaultValues(ticketInfo);
-      //@ts-ignore
-      console.log(this.ticketForm.controls.tickets['normal']);
     });
-  }
-  ngOnChange() {
-    //@ts-ignore
-    console.log(this.ticketForm.controls.tickets['normal']);
   }
 
   private mapDefaultValues(ticket: TicketInfo[]) {
@@ -54,24 +39,11 @@ export class TicketDetails2Component {
     this.ticketType = type;
   }
 
-  private createForm() {
-    return this.builder.group({
-      tickets: this.builder.array([]),
-    });
-  }
-  get tickets() {
-    return this.ticketForm.get('tickets');
+  selectOnChange(event: { value: number; type: string }) {
+    console.log('hit', event);
   }
 
-  submitForm() {
-    console.log('hit');
-    this.ticketForm.markAllAsTouched();
-
-    if (this.ticketForm.invalid) {
-      return;
-    }
-
-    // handle...
-    console.log(this.ticketForm.value);
+  toggleDropdown() {
+    this.hide = !this.hide;
   }
 }
