934df9db268cda587739005a1413d36aa71aeefc
Łukasz Staniszewski <l.staniszewski01@gmail.com>
2023-02-22 15:23:34 +0100

    feat: add movie form

---
+++ src/app/core/movie/movie.api.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { API, MESSAGE } from "src/environments/constants";
+
+type Actors = {
+  name: string;
+  image: string;
+};
+
+export interface Movie {
+  title: string;
+  image: string;
+  genre: string;
+  pg: string;
+  director: string;
+  description: string;
+  actors: string[];
+  runTime: number;
+  premiere: boolean;
+}
+export interface MovieDto extends Movie {
+  id: string;
+  descriptionExtra: {
+    actors: Actors[];
+  };
+  rating: string;
+  votesNumber: number;
+
+  runTime: number;
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class MovieApiService {
+  private http = inject(HttpClient);
+
+  addToDB(movie: Movie) {
+    return this.http.post(API.MOVIES, movie);
+  }
+}
--- src/app/domains/admin/add-movie/add-movie.component.ts
+++ src/app/domains/admin/add-movie/add-movie.component.ts
@@ -1,7 +1,8 @@
 import { COMMA, ENTER } from "@angular/cdk/keycodes";
 import { Component, inject } from "@angular/core";
-import { NonNullableFormBuilder } from "@angular/forms";
+import { NonNullableFormBuilder, Validators } from "@angular/forms";
 import { MatChipInputEvent } from "@angular/material/chips";
+import { MovieApiService } from "@core/movie/movie.api.service";
 
 @Component({
   selector: "app-add-movie",
@@ -12,21 +13,25 @@ export class AddMovieComponent {
   separatorKeysCodes: number[] = [ENTER, COMMA];
   actors = ["jeden", "dwa"];
   private builder = inject(NonNullableFormBuilder);
-
+  private movieApiService = inject(MovieApiService);
   addMovieForm = this.createForm();
 
   createForm() {
     return this.builder.group({
-      imageLink: this.builder.control(""),
-      movieTitle: this.builder.control(""),
-      runTime: this.builder.control(""),
-      pg: this.builder.control(""),
-      actors: this.builder.control(""),
-      genre: this.builder.control(""),
-      director: this.builder.control(""),
-      premiere: this.builder.control(""),
-      description: this.builder.control(""),
+      image: this.builder.control("", [Validators.required]),
+      title: this.builder.control("", [Validators.required]),
+      runTime: this.builder.control(0, [Validators.required]),
+      premiere: this.builder.control(false, [Validators.requiredTrue]),
+      actors: this.builder.control(null, [Validators.required]),
+      genre: this.builder.control("", [Validators.required]),
+      director: this.builder.control("", [Validators.required]),
+      pg: this.builder.control("", [Validators.required]),
+      description: this.builder.control("", [Validators.required]),
     });
+  }
+
+  get controls() {
+    return this.addMovieForm.controls;
   }
   add(event: MatChipInputEvent) {
     const value = (event.value || "").trim();
@@ -34,8 +39,7 @@ export class AddMovieComponent {
       this.actors.push(value);
     }
     event.chipInput.clear();
-    this.addMovieForm.controls.actors.setValue("");
-    console.log("actor add", event);
+    // this.addMovieForm.controls.actors.setValue([]);
   }
   remove(actor: string) {
     const index = this.actors.indexOf(actor);
@@ -43,6 +47,10 @@ export class AddMovieComponent {
     if (index >= 0) {
       this.actors.splice(index, 1);
     }
-    console.log("Actor", actor);
+  }
+  submit() {
+    this.addMovieForm.markAllAsTouched();
+    if (this.addMovieForm.invalid) return;
+    // this.movieApiService.addToDB(this.addMovieForm.getRawValue());
   }
 }
